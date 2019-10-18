8b11ae7bd8d5cc5ac986df52e2365fdde3d4c7ea
Abhinandan02 <56708083+Abhinandan02@users.noreply.github.com>
2019-10-18 19:58:25 +0530

    About page code
    code for about page

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us</title>
+          <!-- #CSS ONLY -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+    crossorigin="anonymous">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="jquerycdn.js"></script>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<style type="text/css">
+  
+body{
+  background-image: url(http://www.joburgchiropractor.co.za/images/background.jpg);
+}
+
+
+
+
+
+
+
+</style>
+
+  <!-- #JS AND JQUERRY -->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+    crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="vendors/linericon/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+
+<section class="about_area lite_bg">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-5 col-md-5">
+					<div class="about_details lite_bg">
+						<h2>Welcome to Medicare Center</h2>
+						<p>Hello!! all
+							<br>
+							The main motto of our organisation is to help to those people who are in need. In this organisation we have online registration sustem for ticket
+							and for those people who stand in a queue for a long time and doesnot gets ticket.
+							So we are here to help you out. We have tickets for all departments and even for outdoor and emergency ward. You can book ticket for patient 
+							and dont need to stand in a queue for a hall ticket which helps to save time and make your work easy.
+						</p>
+						<ul class="list_wrap">
+							<li class="about_lists">People faces higher conduct standard especially in workplace and they sometimes dont reach hopsital in time to 
+								make tickets so its for them
+							</li>
+							<li class="about_lists">You can book ticket anytime and at any place</li>
+						
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-4 offset-lg-3 col-md-6 offset-md-1">
+					<div class="about_right overlay">
+						<div class="about_inner">
+							<h4>Doctors Time table</h4>
+							<p>Mon to Friday -- 07.00 AM to 10.00 PM</p>
+							<p>Mon to Friday -- 07.00 AM to 10.00 PM</p>
+							<p>Mon to Friday -- 07.00 AM to 10.00 PM</p>
+							<p>Mon to Friday -- 07.00 AM to 10.00 PM</p>
+							<p>Mon to Friday -- 07.00 AM to 10.00 PM</p>
+							<p>Mon to Friday -- 07.00 AM to 10.00 PM</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="about_bg overlay"></div>
+		</div>
+    </section>
+</body>
+</html>
